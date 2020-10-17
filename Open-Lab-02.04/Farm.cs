39e3263cef1449b60018d93792a09a181d6826d7
Martin Staňo <martinmatostano@gmail.com>
2020-10-17 16:44:34 +0200

    Add implementation of method GetLegsCount of returns a number of legs.

--- Open-Lab-02.04/Farm.cs
+++ Open-Lab-02.04/Farm.cs
@@ -6,7 +6,7 @@ namespace Open_Lab_02._04
     {
         public int GetLegsCount(int chickens, int cows, int pigs)
         {
-            throw new NotImplementedException();
+            return 2 * chickens + 4 * (cows + pigs);
         }
     }
 }
